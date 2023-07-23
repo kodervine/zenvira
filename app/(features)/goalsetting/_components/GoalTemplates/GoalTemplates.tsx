c1e323a3f85104f4e892505349158efb4dee3574
Chinenye Anikwenze <anikwenzekelly@gmail.com>
2023-07-23 13:42:03 +0100

    chore: deleting unused components

--- app/(features)/goalsetting/_components/GoalTemplates/GoalTemplates.tsx
+++
@@ -1,3 +0,0 @@
-export const GoalTemplates = () => {
-  return <div>GoalTemplates</div>;
-};
--- app/(features)/goalsetting/page.tsx
+++ app/(features)/goalsetting/page.tsx
@@ -3,17 +3,16 @@ import { useState } from "react";
 import {
   ExistingGoalDataTable,
   GoalSettingForm,
-  GoalSubmissionSuccess,
 } from "@/app/(features)/goalsetting";
 
 const GoalSettingTool = () => {
   const [isGoalSubmitted, setIsGoalSubmitted] = useState(false);
   return (
-    <div className="bg-bgColor p-4">
+    <div className="p-4">
       <a href="/" className="mb-4 block">
         Back to home
       </a>
-      <div className="bg-gray-100 mx-auto p-8 rounded-md shadow-md min-h-screen">
+      <div className=" mx-auto p-8 rounded-md shadow-md min-h-screen">
         {/* Title and other elements */}
         <div className="flex flex-col md:flex-row">
           <div className="flex-1 md:pr-0 md:pr-8">
@@ -21,7 +20,7 @@ const GoalSettingTool = () => {
           </div>
           {/* Should show more details about each goal by the side when a row from the table above is clicked */}
           <div className="flex-2 md:pl-0 md:pl-8">
-            {!isGoalSubmitted ? <GoalSettingForm /> : <GoalSubmissionSuccess />}
+            {!isGoalSubmitted ? <GoalSettingForm /> : "successful"}
           </div>
         </div>
       </div>
