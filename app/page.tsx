6d78ef9b9f13c36999f206133deeef5249d251be
Chinenye Anikwenze <anikwenzekelly@gmail.com>
2023-08-10 19:03:42 +0100

    fix: deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import GoalSettingPage from "@/app/(features)/goalsetting/page";
+import DashboardPage from "@/app/(features)/dashboard/page";
 
 // todo - probably have to link to a landing page. But for now, should be the dashboard
 export default function Home() {
-  return <GoalSettingPage />;
+  return <DashboardPage />;
 }
